4d171bdd019bda2f7ae32c726c2368fb89f409d4
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-23 23:33:31 +0400

    add

---
+++ IAccount_task/IAccount_task/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IAccount_task.Models
+{
+    public class User
+    {
+        public static string Id = "^[0-9]{1,}$";
+        public static string Fullname = "^([a-zA-Z]{3,})$";
+        public static string Email = @"^([^\W])([a-z\d\.\-_]+)@([a-z\d\-]+\.)([a-z]{2,13})$";
+        public static string Password = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])+[a-zA-z0-9]{8,}$";
+
+        public bool IdCheckher()
+        {
+            return Regex.IsMatch("1234", Id);
+        }
+        public bool FullnameCheckher()
+        {
+            return Regex.IsMatch("Aysou", Fullname);
+        }
+        public bool EmailCheckher()
+        {
+            return Regex.IsMatch("aysumamedova26@gmail.com", Email);
+        }
+        public bool PasswordCheckher()
+        {
+            return Regex.IsMatch("A7a7952323332", Password);
+        }
+
+
+
+    }
+}
---
+++ IAccount_task/IAccount_task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IAccount_task.Models;
+
+namespace IAccount_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User Regex = new User();
+
+            bool Id = Regex.IdCheckher();
+            bool Fullname = Regex.FullnameCheckher();
+            bool Email = Regex.EmailCheckher();
+            bool Password = Regex.PasswordCheckher();
+
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Fullname: { Fullname}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Password {Password}");
+        }
+    }
+}
